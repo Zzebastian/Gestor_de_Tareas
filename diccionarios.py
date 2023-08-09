9d7aab1f3b4b3b417142e911b7b425ac9916911c
sebastian.scheverin@gmail.com <Zebastian@Loc-OS-Zeba>
2023-08-09 10:10:20 -0300

    funciones iniciales

---
+++ diccionarios.py
@@ -0,0 +1,7 @@
+
+task = {'Proyecto': 'Ingresar Nombre de proyecto: ',
+        'Github': 'Ingresar web de Github: ',
+        'Estado': 'Ingresar estado proyecto: ',
+        'Tareas próximas': 'Ingresar tareas siguiesntes: ',
+        'Notas': 'Si hay notas, agréguelas: ',
+        'Consigna': 'Ingrese la consigna del proyecto: '}
--- funciones.py
+++ funciones.py
@@ -1,1 +1,29 @@
 # Este archivo se crea de la terminal
+
+from diccionarios import *
+
+listaProyectos = []
+elemento = {}
+
+def tarea():
+    global elemento
+    for key in task:
+        elemento[key] = input(task[key])
+    return elemento
+#
+
+def agregarProyecto():
+    # Agrega elementos a la lista de proyectos
+    global listaProyectos
+    elemento = tarea()
+    listaProyectos.append(elemento)
+#
+
+def imprimirProyectos():
+    global listaProyectos
+    for proy in listaProyectos:
+        for key in task:
+            print(proy[key])
+        print('\n@@@@@@@@\n')
+#
+
